bad2afcd709ef733ebacefda9c093cd44ea8cc76
Nilam Pathak <nilampathak72@gmail.com>
2020-06-19 23:14:42 +0530

    final commit: styles added

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ $("ul").on("click", "li", function(){
 
 $("ul").on("click", "span", function(event){
     $(this).parent().fadeOut(600, function(){
-        $(this).remove();
+        $(this).remove;
     });
     event.stopPropagation();
 });
@@ -13,6 +13,10 @@ $("input[type='text']").keypress(function(event){
     if(event.which === 13){
         var newTodo = $(this).val();
         $(this).val("");
-        $("ul").append("<li> <span>X</span> " + newTodo + "</li>");
+        $("ul").append("<li> <span><i class='fas fa-trash-alt'></i></span> " + newTodo + "</li>");
     }
+});
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
 });
